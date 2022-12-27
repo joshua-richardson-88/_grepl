0461684351467e42ed11c2e73019cb46b0a830b2
Joshua Richardson <josh@Joshuas-MacBook-Pro.local>
2022-12-26 18:09:18 -0800

    fix(trpc): Forgot to use prisma client from context

--- src/server/trpc/router/words.ts
+++ src/server/trpc/router/words.ts
@@ -2,7 +2,7 @@ import { router, publicProcedure } from "../trpc"
 import { z } from "zod"
 
 export const wordRouter = router({
-  isValidWord: publicProcedure.input(z.string()).mutation(({ input }) => {
-    return prisma?.word.findFirst({ where: { word: input } })
+  isValidWord: publicProcedure.input(z.string()).mutation(({ ctx, input }) => {
+    return ctx.prisma.word.findFirst({ where: { word: input } })
   }),
 })
